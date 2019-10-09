da69bf9958eb2ca4793e6d96a5b2ee65a6bdb842
alexisdebeauvais <alexisdebeauvais@gmail.com>
2019-10-09 21:04:17 +0200

    creating the phrase class and its attribute

---
+++ src/main/java/com/bellang/back/model/Phrase.java
@@ -0,0 +1,13 @@
+package com.bellang.back.model;
+
+
+public class Phrase {
+
+    private String ID;
+    private String content;
+    private String language;
+    private int ordre;
+    private String format;
+    private Affichage affichage;
+
+}
