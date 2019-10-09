f8675a26a66bdaa919027e835aed2341f66fbf01
alexisdebeauvais <alexisdebeauvais@gmail.com>
2019-10-09 20:19:55 +0200

    service creation with simple sysout

---
+++ src/main/java/com/bellang/back/service/ServiceEssai.java
@@ -0,0 +1,14 @@
+package com.bellang.back.service;
+
+import org.springframework.stereotype.Service;
+
+import java.util.logging.Logger;
+
+@Service
+public class ServiceEssai {
+
+    public void methodEssai(){
+        System.out.println("HELLO TU ES DS UN SERVICE");
+    }
+
+}
