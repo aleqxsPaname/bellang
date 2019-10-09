86740d5b2a57c422bb4d4596ec5e225b20d2ef32
alexisdebeauvais <alexisdebeauvais@gmail.com>
2019-10-09 21:25:31 +0200

    adding affichage model

---
+++ src/main/java/com/bellang/back/model/Affichage.java
@@ -0,0 +1,8 @@
+package com.bellang.back.model;
+
+public class Affichage{
+
+    String position;
+    int dureeEnMillisecond;
+
+}
---
+++ src/main/java/com/bellang/back/model/AffichageDiapo.java
@@ -0,0 +1,10 @@
+package com.bellang.back.model;
+
+public class AffichageDiapo {
+
+    String position;
+
+    public String compute(){
+        return this.position;
+    }
+}
