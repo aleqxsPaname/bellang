4ceffef3f7f59f87b477682e6c509e0a559d6e45
alexisdebeauvais <alexisdebeauvais@gmail.com>
2019-10-10 19:57:26 +0200

    simplifying pom for a simple CommandLineRunner application

--- src/main/java/com/bellang/back/Application.java
+++ src/main/java/com/bellang/back/Application.java
@@ -2,23 +2,27 @@ package com.bellang.back;
 
 import com.bellang.back.service.ServiceEssai;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.swing.*;
+
 @SpringBootApplication
-public class Application {
+public class Application implements CommandLineRunner {
 
     @Autowired
     private ServiceEssai serviceEssai;
 
     public static void main(String[] args) {
         System.out.println("TEST D AFFICHAGE");
-        //run();
-        SpringApplication.run(Application.class, args);
+
+        SpringApplication app = new SpringApplication(Application.class);
+        app.run(args);
     }
 
 
-    private void run(String... args) {
+    public void run(String... args) {
         System.out.println("OK FONCTIONNE ON EST DS RUN DE APPLICATION");
         serviceEssai.methodEssai();
     }
