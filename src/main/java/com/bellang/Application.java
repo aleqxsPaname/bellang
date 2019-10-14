89a951b64c6bfaa1cac7b6429ae0a1213e7206bd
alexisdebeauvais <alexisdebeauvais@gmail.com>
2019-10-14 21:38:05 +0200

    Doing a test of phrase findAll()

--- src/main/java/com/bellang/Application.java
+++ src/main/java/com/bellang/Application.java
@@ -1,13 +1,22 @@
 package com.bellang;
 
+import com.bellang.model.entity.Phrase;
+import com.bellang.model.repository.PhraseRepository;
 import com.bellang.service.ServiceEssai;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.List;
+
 @SpringBootApplication
 public class Application implements CommandLineRunner {
+
+    // TODO a mettre en test et dans un service pas directement ds le main
+    @Autowired
+    private PhraseRepository repository;
+
 
     @Autowired
     private ServiceEssai serviceEssai;
@@ -19,6 +28,13 @@ public class Application implements CommandLineRunner {
 
     public void run(String... args) {
         System.out.println("DISPLAY AFTER SPRING CONTEXT LOADING");
+        repository.save(new Phrase("blabla", "FR"));
+
+        List<Phrase> phrases = repository.findAll();
+        for(Phrase phrase:phrases){
+            System.out.println("phrase => " + phrase.getContent());
+        }
+
         serviceEssai.methodEssai();
     }
 
